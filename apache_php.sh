b2e1a86b721f3d157b318f8ff9520cd81eecdaae
joeyjesky <47004259+joeyjesky@users.noreply.github.com>
2019-05-10 14:48:22 -0500

    Update apache_php.sh

--- apache_php.sh
+++ apache_php.sh
@@ -18,7 +18,7 @@ echo "ServerName $PUB_IP" >> /etc/apache2/apache2.conf
 #Installing PHP and required modules
 echo "Installing php modules"
 echo "deb http://ftp.de.debian.org/debian stretch main" >> /etc/apt/sources.list
-apt-get -y install php7.0-fpm php7.0-cli php7.0-common libapache2-mod-php7.0 php7.0-mcrypt php7.0-mysql php5-mysqlnd
+sudo apt-get -y install php7.0-fpm php7.0-cli php7.0-common libapache2-mod-php7.0 php7.0-mcrypt php7.0-mysql php5-mysqlnd
 
 #Setting permissions and ownership for /var/www
 echo "Setting ownership on /var/www"
