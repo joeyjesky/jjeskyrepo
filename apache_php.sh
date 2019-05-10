809e17e094f0b503419ac940c4aaa7364d8b5321
joeyjesky <47004259+joeyjesky@users.noreply.github.com>
2019-05-10 15:16:40 -0500

    Update apache_php.sh

--- apache_php.sh
+++ apache_php.sh
@@ -18,7 +18,8 @@ echo "ServerName $PUB_IP" | sudo tee -a /etc/apache2/apache2.conf
 
 #Installing PHP and required modules
 echo "Installing php modules"
-echo "deb http://ftp.de.debian.org/debian stretch main" >> /etc/apt/sources.list
+#echo "deb http://ftp.de.debian.org/debian stretch main" >> /etc/apt/sources.list
+echo "deb http://ftp.de.debian.org/debian stretch main" | sudo tee -a /etc/apt/sources.list
 sudo apt-get -y install php7.0-fpm php7.0-cli php7.0-common libapache2-mod-php7.0 php7.0-mcrypt php7.0-mysql php5-mysqlnd
 
 #Setting permissions and ownership for /var/www
